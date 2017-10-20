a98fc611f3455eba95415376c4dcdb6541d8c011
Lynus <lynusz@umich.edu>
2017-10-20 03:22:22 -0400

    pt2

--- Rocket/ViewController.swift
+++ Rocket/ViewController.swift
@@ -22,7 +22,6 @@ class ViewController: UIViewController {
     var player: AVAudioPlayer!
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
